62f74bac1c277715d25b081051c989924be0dcad
InTostor <gena05irk@yandex.ru>
2023-10-20 18:37:27 +0800

    .gitignore fix fix

--- scripts/bulkDownloader/load.py
+++ scripts/bulkDownloader/load.py
@@ -5,7 +5,7 @@ import atexit
 
 
 MAXPROCESSES = 12
-COOKIE = "3:1696148539.5.0.1696148539996:BfR1sA:8f.1.2:1|1777946379.-1.0.1:334934056.3:1696148539|3:10276549.570400.zKfy0CuOPwfG-0iOPQO5hTx58rE"
+COOKIE = "3:1697553124.5.0.1697553124185:ufR1sA:1.1.2:1|1777946379.-1.0.1:334934056.3:1697553124|3:10277329.348059._oa1GsiRoaBbnnXt6Yug4qAhLc8"
 
 t=time.time()
 
@@ -13,7 +13,7 @@ urlsFile = open("urls.txt")
 urls = urlsFile.readlines()
 urlsFile.close()
 
-def runPerlDownloader(url,delay=1,bitrate=320,dir="~/Music"):
+def runPerlDownloader(url,delay=2,bitrate=320,dir="~/Temp"):
   cmd = f"perl ya.pl --bitrate {bitrate} --skip-existing -u '{url}' --cookie '{COOKIE}' --dir {dir} --delay {delay}"
   subprocess.run(cmd,shell=True)
 
