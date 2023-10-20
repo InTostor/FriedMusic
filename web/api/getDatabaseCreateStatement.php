e40850dcbe90b8bf1bdfeb3459e06727455b3e5e
InTostor <gena05irk@yandex.ru>
2023-10-20 18:59:25 +0800

    last method fix

--- web/api/getDatabaseCreateStatement.php
+++ web/api/getDatabaseCreateStatement.php
@@ -4,9 +4,16 @@ require_once "$root/lib/dbWrapper.php";
 require_once "$root/lib/dev.php";
 require_once "$root/settings/config.php";
 
+if (isset($_GET['typesonly'])){
+  $sql = "DESCRIBE $trackMetadataTable";
+  $answer = Database::executeStmt($sql);
+  header("Content-Type: application/json");
+  echo json_encode($answer);
+}else{
+  $sql = "show create table $trackMetadataTable";
+  $answer = Database::executeStmt($sql);
+  header("Content-Type: text/plain");
+  echo $answer[0]['Create Table'];
+}
 
-$sql = "show create table $trackMetadataTable";
 
-$answer = Database::executeStmt($sql);
-header("text/plain");
-echo $answer[0]['Create Table'];
